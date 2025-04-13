1e1a77f46fe25b6902a49225b155d6b8cad30849
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-04-14 03:21:22 +0530

    Try

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type React from "react";
 import type { Metadata, Viewport } from "next/types";
 import { Inter } from "next/font/google";
 import Script from "next/script";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BootstrapClient from "./bootstrap-client";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,23 +29,24 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Add Bootstrap CSS directly from CDN */}
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
           integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
           crossOrigin="anonymous"
-        />
+        /> */}
       </head>
       <body className={inter.className}>
+        <BootstrapClient />
         {children}
 
         {/* Add Bootstrap JS directly from CDN */}
-        <Script
+        {/* <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
           crossOrigin="anonymous"
           strategy="afterInteractive"
-        />
+        /> */}
       </body>
     </html>
   );
